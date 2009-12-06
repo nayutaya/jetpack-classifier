5d513357a33042062f871ac8138f8949cacdfbd4
Yuya Kato <yuyakato@gmail.com>
2009-12-06 13:02:01 +0900

    add classifier

---
+++ server/dictionary.rb
@@ -0,0 +1,66 @@
+#! ruby -Ku
+
+# オブジェクトとID（1～）を相互に変換する辞書
+
+class Dictionary
+  def initialize(hash = {})
+    @obj2id = {}
+    @id2obj = {}
+    hash.each { |obj, id|
+      @obj2id[obj] = id
+      @id2obj[id]  = obj
+    }
+  end
+
+  def to_hash
+    return @obj2id.dup
+  end
+
+  def encode(obj)
+    return @obj2id[obj] ||
+      begin
+        id = @obj2id.size + 1
+        @id2obj[id]  = obj
+        @obj2id[obj] = id
+        id
+      end
+  end
+
+  def encode_multiple(collection)
+    case collection
+    when Array
+      return collection.map { |obj|
+        self.encode(obj)
+      }
+    when Hash
+      return collection.inject({}) { |memo, (obj, value)|
+        memo[self.encode(obj)] = value
+        memo
+      }
+    else raise(ArgumentError)
+    end
+  end
+
+  def decode(id)
+    return @id2obj[id]
+  end
+
+  def decode_multiple(collection)
+    case collection
+    when Array
+      return collection.map { |id|
+        self.decode(id)
+      }
+    when Hash
+      return collection.inject({}) { |memo, (id, value)|
+        memo[self.decode(id)] = value
+        memo
+      }
+    else raise(ArgumentError)
+    end
+  end
+
+  def ids
+    return @id2obj.keys
+  end
+end
---
+++ server/naive_bayes_categorizer.rb
@@ -0,0 +1,68 @@
+
+# 単純ベイズ分類器を用いたカテゴリ分類器
+
+$: << File.join(File.dirname(__FILE__), "..")
+require "naive_bayes_classifier"
+require "dictionary"
+require "bigram_tokenizer"
+
+class NaiveBayesCategorizer
+  def initialize(tokenizer, hash = {})
+    @tokenizer = tokenizer
+
+    @category_dictionary = Dictionary.new(hash[:category_dictionary] || {})
+    @feature_dictionary  = Dictionary.new(hash[:feature_dictionary] || {})
+    @classifier          = NaiveBayesClassifier.new(hash[:classifier] || {})
+  end
+
+  attr_reader :category_dictionary, :feature_dictionary
+
+  def self.load(tokenizer, filepath)
+    bin  = File.open(filepath, "rb") { |file| file.read }
+    hash = Marshal.load(bin)
+    return self.new(tokenizer, hash)
+  end
+
+  def to_hash
+    return {
+      :category_dictionary => @category_dictionary.to_hash,
+      :feature_dictionary  => @feature_dictionary.to_hash,
+      :classifier          => @classifier.to_hash,
+    }
+  end
+
+  def save(filepath)
+    hash = self.to_hash
+    bin  = Marshal.dump(hash)
+    File.open(filepath, "wb") { |file| file.write(bin) }
+  end
+
+  def train(category, document)
+    features    = @tokenizer.tokenize(document)
+    category_id = @category_dictionary.encode(category)
+    feature_ids = @feature_dictionary.encode_multiple(features)
+    @classifier.train(category_id, feature_ids)
+  end
+
+  def classify(document)
+    features    = @tokenizer.tokenize(document)
+    feature_ids = @feature_dictionary.encode_multiple(features)
+    probs = @classifier.classify(feature_ids)
+    return @category_dictionary.decode_multiple(probs)
+  end
+
+  def categorize(document, thresholds = {})
+    probs = self.classify(document).
+      map     { |category, prob| [category, prob] }.
+      sort_by { |category, prob| prob }
+
+    first_category,  first_prob  = probs[-1]
+    second_category, second_prob = probs[-2]
+
+    if first_prob > second_prob * (thresholds[first_category] || 1.0)
+      return first_category
+    else
+      return nil
+    end
+  end
+end
