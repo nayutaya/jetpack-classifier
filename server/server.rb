d23a1108cdcfd57aaf2a770f4453990510b4cf70
Yuya Kato <yuyakato@gmail.com>
2009-12-06 13:39:44 +0900

    add categorize process

--- server/server.rb
+++ server/server.rb
@@ -3,13 +3,39 @@ require "rubygems"
 require "sinatra"
 require "json"
 
+require "naive_bayes_categorizer"
+
+thresholds = {
+  "rail" => 1.0,
+  "rest" => 3.5,
+}
+
+tokenizer   = BigramTokenizer.new
+categorizer = NaiveBayesCategorizer.load(tokenizer, "out.full.db")
+
+process = proc {
+  #data = JSON.parse(params[:data])
+
+  title = "愛知・貸金業者遺体発見　遺棄容疑で男逮捕　殺害も捜査"
+  title = "ＪＲ西歴代３社長を再び不起訴　宝塚線事故で神戸地検"
+  category = categorizer.categorize(title, thresholds) || "unknown"
+
+  result = {
+    "category" => category,
+  }
+
+  #content_type :json
+  JSON.unparse(result)
+}
+
+p process[]
+
+=begin
 get "/" do
-  ""
+  process[]
 end
 
 post "/" do
-  data = params[:data]
-  result = {1 => data}
-  content_type :json
-  JSON.unparse(result)
+  process[]
 end
+=end
