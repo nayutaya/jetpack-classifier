ae33593c9ca0968d5b34468ffbe31e7dfcf2bf0a
Yuya Kato <yuyakato@gmail.com>
2009-12-06 13:46:50 +0900

    categorize

--- server/server.rb
+++ server/server.rb
@@ -6,36 +6,30 @@ require "json"
 require "naive_bayes_categorizer"
 
 thresholds = {
-  "rail" => 1.0,
-  "rest" => 3.5,
+  "a" => 1.0,
+  "b" => 3.5,
 }
 
 tokenizer   = BigramTokenizer.new
 categorizer = NaiveBayesCategorizer.load(tokenizer, "out.full.db")
 
-process = proc {
-  #data = JSON.parse(params[:data])
-
-  title = "愛知・貸金業者遺体発見　遺棄容疑で男逮捕　殺害も捜査"
-  title = "ＪＲ西歴代３社長を再び不起訴　宝塚線事故で神戸地検"
-  category = categorizer.categorize(title, thresholds) || "unknown"
-
-  result = {
-    "category" => category,
+process = proc { |records|
+  records.each { |record|
+    title = record["title"].strip
+    unless title.empty?
+      category = categorizer.categorize(title, thresholds) || "unknown"
+      record["visible"] = (category == "a")
+    end
   }
-
-  #content_type :json
-  JSON.unparse(result)
+  records
 }
 
-p process[]
-
-=begin
 get "/" do
-  process[]
+  content_type :json
+  JSON.unparse(process[JSON.parse(params[:data])])
 end
 
 post "/" do
-  process[]
+  content_type :json
+  JSON.unparse(process[JSON.parse(params[:data])])
 end
-=end
