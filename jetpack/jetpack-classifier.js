ff54bc95da740f2a11a4d29f952d159061d6f557
hitode909 <hitode909@gmail.com>
2009-12-06 15:14:50 +0900

    slidebar(no japanese)

--- jetpack/jetpack-classifier.js
+++ jetpack/jetpack-classifier.js
@@ -1,9 +1,12 @@
-jetpack.tabs.onReady(function() {
+jetpack.future.import("slideBar");
+
+
+    jetpack.tabs.onReady(function() {
     var doc = jetpack.tabs.focused.contentDocument;
     var elements = [];
     jetpack.notifications.show("preparing");
-    $(doc).find("a").each(function() {
-        var url = $(this).attr("href");
+        $(doc).find("a").each(function() {
+        var url = this.href;
         var title = $(this).text();
         if (typeof(title) == "string" && title.length > 0 ) {
             elements.push({ url: url, title: title});
@@ -16,11 +19,28 @@ jetpack.tabs.onReady(function() {
         data: {data: uneval(elements)},
         success: function(data) {
             jetpack.notifications.show("success");
+            var visibles = [];
             $(data, doc).each( function() {
-                if( !this.visible) {
-                    $(doc).find("a[href=" + this.url + "]").css("color", "#DDDDDD");
+                $(doc).find("a[href=" + this.url + "]").css("font-size", this.visible ? "150%" : "50%");
+                if (this.visible) visibles.push(this);
+            });
+            var content = "<h1>JR</h1>";
+            console.log(uneval(visibles));
+            $(data, doc).each(function() {
+                content += "<div><a href='" + decodeURIComponent(this.url) + "'>" + this.title + "</a></div>";
+            });
+            jetpack.slideBar.append({
+                width: 250,
+                icon: 'http://www.kix.in/misc/jetpacks/notes.png',
+                html: content,
+                onReady: function(slide) {
+                    cb = slide;
+                },
+                onSelect: function(slide) {
+                    jetpack.notifications.show('onSelect');
                 }
             });
+
             },
         error: function(res) {
             jetpack.notifications.show("error");
